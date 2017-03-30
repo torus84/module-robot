caaa69f3bb7a0dc392ab79b1a86a438a2e40b9e7
Taras Shabatyn <tshabatyn@magento.com>
2017-03-30 12:16:00 +0300

    init

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © 2016 Taras Shabatyn. All rights reserved.
+ *
+ * Please contact: taras.shabatin@gmail.com
+ */
+use \Magento\Framework\Component\ComponentRegistrar as Registrar;
+
+Registrar::register(Registrar::MODULE, 'Torus_Robot', __DIR__);
